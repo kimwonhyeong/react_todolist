b1a93731046c47c20a0b327053b9a0cfe173dd37
kimwonhyeong <104440624+kimwonhyeong@users.noreply.github.com>
2022-07-13 15:32:05 +0900

    version2//삭제버튼추가

--- App.js
+++ App.js
@@ -1,9 +1,10 @@
-import { useState,  } from "react";
+//version2 삭제 기능 추가
+import { useState } from "react";
 function App() {
-	const [toDo, setToDo]= useState("");
-	const [toDos, setToDos]= useState([]);
-	const onChange = (event)=> setToDo(event.target.value);
-	const onSubmit = (e)=> {
+	const [toDo, setToDo] = useState("");
+	const [toDos, setToDos] = useState([]);
+	const onChange = (e)=>setToDo(e.target.value);
+	const onSubmit = (e)=>{
 		e.preventDefault();
 		if(toDo===""){
 			return;
@@ -11,23 +12,23 @@ function App() {
 		setToDos((currentArray)=>[toDo,...currentArray]);
 		setToDo("");
 	}
-	console.log(toDos);
+	const onDelete = (index)=>{
+		setToDos(toDos.filter((toDo,toDoIndex)=>index !== toDoIndex));
+	};
 	return (
 		<div>
-			<h1>My To Dos({toDos.length})</h1>
+			<h1>react ToDoList</h1>
 			<form onSubmit={onSubmit}>
 				<input
 				type="text"
 				placeholder="Write your to do..."
 				onChange={onChange}
-				value={toDo}
-				/>
+				value={toDo} />
 				<button>Add To Do</button>
 			</form>
 			<ul>
-				{toDos.map((item,index)=><li key={index}>{item}</li>)}
+				{toDos.map((item,index)=><li key={index}>{item}<button  onClick={()=>onDelete(index)}>x</button></li>)}
 			</ul>
-			
 		</div>
   );
 }
